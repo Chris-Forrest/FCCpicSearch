dc9c194ea25e0e4d87c480c60ae3b30839e67afd
Chris Forrest <42876707+Chris-Forrest@users.noreply.github.com>
2024-07-19 13:09:10 -0400

    Update App.jsx
    created onSubmit function

--- unsplash_image_serch/src/App.jsx
+++ unsplash_image_serch/src/App.jsx
@@ -3,14 +3,18 @@ import { Form } from 'react-bootstrap';
 import './index.css';
 
 const App = () => {
-  
   const searchInput = useRef(null);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log('submitted')
+  }
 
   return (
     <div className='container'>
       <h1 className='title'>Image Search</h1>
       <div className='search-section'>
-        <Form>
+        <Form onSubmit={handleSearch}>
           <Form.Control
             type='search'
             placeholder='Type something to search...'
